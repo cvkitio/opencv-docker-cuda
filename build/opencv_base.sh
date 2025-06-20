7858569e6585faedc4ef06587630ed2167379c2d
Andrew Sinclair <ajsinclair@gmail.com>
2025-06-21 09:14:35 +1000

    Test out python in base

--- build/opencv_base.sh
+++ build/opencv_base.sh
@@ -1,3 +1,3 @@
 docker buildx build --platform linux/amd64 -f dockerfiles/Dockerfile_opencv_base -t ajsinclair/opencv-base:latest --load .
-docker tag ajsinclair/opencv-base ajsinclair/opencv-base:arch7.5
-docker push ajsinclair/opencv-base:latest
+docker tag ajsinclair/opencv-base ajsinclair/opencv-base:latest
+#docker push ajsinclair/opencv-base:latest
